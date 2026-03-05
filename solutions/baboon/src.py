86c28789d58dd6da5f11c006ff92181ccb6653f0
Endre Márk Borza <endremborza@gmail.com>
2026-03-05 21:52:21 +0100

    init baboon

--- solutions/baboon/src.py
+++ solutions/baboon/src.py
@@ -5,20 +5,15 @@ df = pd.read_csv("input.csv")
 query_df = pd.read_csv("query.csv")
 
 out = []
-for idx, row in query_df.iterrows():
-    diffs = (
-        df.loc[
-            df[row["genre"]]
-            & (df["year"] >= row["min_year"])
-            & (df["year"] <= row["max_year"]),
-            ["x", "y"],
-        ]
-        - row
-    )
-    squares = diffs**2
-    dists = squares.sum(axis=1) ** 0.5
-
-    closest_idx = dists.sort_values().index[0]
-    out.append(df.loc[closest_idx, ["year", "title", "imdb_id"]].to_dics())
+for _, row in query_df.iterrows():
+    filtered = df.loc[
+        df[row["genre"]]
+        & (df["year"] >= row["min_year"])
+        & (df["year"] <= row["max_year"]),
+        ["x", "y"],
+    ]
+    dists = ((filtered - row[["x", "y"]]) ** 2).sum(axis=1) ** 0.5
+    closest_idx = dists.idxmin()
+    out.append(df.loc[closest_idx, ["year", "title", "imdb_id"]].to_dict())
 
 pd.DataFrame(out).to_csv("out.csv", index=False)
